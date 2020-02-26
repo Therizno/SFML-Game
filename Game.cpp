536e4122bd42db4a6fb88a9bad0c11729ba05a14
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-02-25 23:50:22 -0500

    Game progress with bug
    Plane texture currently doesn't load

---
+++ Game.cpp
@@ -0,0 +1,105 @@
+#include <SFML/Graphics.hpp>
+
+#include "Game.h"
+
+#include "TextureHolder.cpp"
+
+
+Game::Game() : mWindow(sf::VideoMode(640, 480), "SFML Application"), mTexture(), mPlayer()
+{
+	speed = 75;
+	
+	TextureHolder textures;
+	
+	textures.load(Textures::Airplane, "plane.png");
+	
+	mPlayer.setTexture(textures.get(Textures::Airplane));
+	mPlayer.setPosition(100.0f, 100.0f);
+}
+
+void Game :: run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	
+	while (mWindow.isOpen())
+	{
+		timeSinceLastUpdate += clock.restart();
+		
+		while (timeSinceLastUpdate > TIME_PER_FRAME)
+		{
+			processEvents();
+			update(TIME_PER_FRAME);
+		
+			timeSinceLastUpdate -= TIME_PER_FRAME;
+		}
+		
+		render();
+	}
+}
+
+void Game :: processEvents()
+{
+	sf::Event event;
+	
+	while (mWindow.pollEvent(event))
+	{
+		switch (event.type)
+		{
+			case sf::Event::KeyPressed:
+				handlePlayerInput(event.key.code, true);
+				break;
+			case sf::Event::KeyReleased:
+				handlePlayerInput(event.key.code, false);
+				break;
+			case sf::Event::Closed:
+				mWindow.close();
+				break;
+		}
+
+	}
+}
+
+void Game::update(sf::Time deltaTime)
+{
+	sf::Vector2f movement(0.0f, 0.0f);
+	
+	if(mIsMovingUp)
+		movement.y -= speed;
+	if(mIsMovingLeft)
+		movement.x -= speed;
+	if(mIsMovingRight)
+		movement.x += speed;
+	if(mIsMovingDown)
+		movement.y += speed;
+		
+	mPlayer.move(movement * deltaTime.asSeconds());
+}
+
+void Game::render()
+{
+	mWindow.clear();
+	mWindow.draw(mPlayer);
+	mWindow.display();
+}
+
+
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+	switch (key) 
+	{ 
+		case sf::Keyboard::W:
+			mIsMovingUp = isPressed;
+			break;
+		case sf::Keyboard::A:
+			mIsMovingLeft = isPressed;
+			break;
+		case sf::Keyboard::D:
+			mIsMovingRight = isPressed;
+			break;
+		case sf::Keyboard::S:
+			mIsMovingDown = isPressed;
+			break;
+	}
+}
+
---
+++ Game.h
@@ -0,0 +1,24 @@
+
+class Game
+{
+	public:
+		Game();
+		void run();
+		
+	private:
+		void processEvents();
+		void update(sf::Time deltaTime);
+		void render();
+		void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+		
+	private:
+		bool mIsMovingUp;
+		bool mIsMovingLeft;
+		bool mIsMovingRight;
+		bool mIsMovingDown; 
+		float speed;
+		const sf::Time TIME_PER_FRAME = sf::seconds(1.0f/60.0f);
+		sf::RenderWindow mWindow;
+		sf::Texture mTexture;
+		sf::Sprite mPlayer;
+};
