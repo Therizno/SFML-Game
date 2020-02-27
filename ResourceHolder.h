4c8ef06f4318490314de4b14d00ae9e6441090df
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-02-27 01:56:26 -0500

    Added ResourceHolder load() overload method with second param

--- ResourceHolder.h
+++ ResourceHolder.h
@@ -20,6 +20,21 @@ class ResourceHolder
 			assert(inserted.second); 
 		}
 		
+		template <typename Parameter>
+   		void load(Identifier id,
+   		const std::string& filename, const Parameter& secondParam)
+   		{
+   			std::unique_ptr<Resource> resource(new Resource());
+	
+			if(!resource -> loadFromFile(filename, secondParam))
+			{
+				throw std::runtime_error("ResourceHolder::load - Failed to load "+filename);
+			}
+	
+			auto inserted = mResourceMap.insert(std::make_pair(id, std::move(resource))); 
+			assert(inserted.second); 
+   		}
+		
 		
 		Resource& get(Identifier id)
 		{
