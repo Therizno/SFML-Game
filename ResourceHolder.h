39f45374ab086924b4d17b53fa6fa82022f8e408
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-02-27 01:49:56 -0500

    Revert "Temporary fix"
    This reverts commit 2f1a7cdad2754831918a6dce6da767d19a5c5772.

--- ResourceHolder.h
+++ ResourceHolder.h
@@ -7,36 +7,9 @@ template <typename Resource, typename Identifier>
 class ResourceHolder
 {
 	public:
-		void load(Identifier id, const std::string& filename)
-		{
-			std::unique_ptr<Resource> resource(new Resource());
-	
-			if(!resource -> loadFromFile(filename))
-			{
-				throw std::runtime_error("ResourceHolder::load - Failed to load "+filename);
-			}
-	
-			auto inserted = mResourceMap.insert(std::make_pair(id, std::move(resource))); 
-			assert(inserted.second); 
-		}
-		
-		
-		Resource& get(Identifier id)
-		{
-    		auto found = mResourceMap.find(id);
-    		assert(found != mResourceMap.end());
-
-    		return *found->second; 
-		}
-		
-		
-		const Resource& get(Identifier id) const
-		{
-    		auto found = mResourceMap.find(id);
-    		assert(found != mResourceMap.end());
-
-    		return *found->second; 
-		}
+		void load(Identifier id, const std::string& filename);
+		Resource& get(Identifier id);
+		const Resource& get(Identifier id) const;
 		
 
 	private:
