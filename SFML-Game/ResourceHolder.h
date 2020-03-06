d964aba32b2c1c7dca30719b7d9c10fe62a88c29
Chief Nelson <chief@Chiefs-MacBook-Pro.local>
2020-03-05 19:33:27 -0500

    Moved ResourceHolder functions to cpp file

--- SFML-Game/ResourceHolder.h
+++ SFML-Game/ResourceHolder.h
@@ -12,52 +12,13 @@ template <typename Resource, typename Identifier>
 class ResourceHolder
 {
 	public:
-		void load(Identifier id, const std::string& filename)
-		{
-			std::unique_ptr<Resource> resource(new Resource());
-	
-			if(!resource -> loadFromFile(resourcePath() + filename))
-			{
-				throw std::runtime_error("ResourceHolder::load - Failed to load "+filename);
-			}
-	
-			auto inserted = mResourceMap.insert(std::make_pair(id, std::move(resource))); 
-			assert(inserted.second); 
-		}
-		
-		template <typename Parameter>
-   		void load(Identifier id,
-   		const std::string& filename, const Parameter& secondParam)
-   		{
-   			std::unique_ptr<Resource> resource(new Resource());
-	
-			if(!resource -> loadFromFile(filename, secondParam))
-			{
-				throw std::runtime_error("ResourceHolder::load - Failed to load "+filename);
-			}
-	
-			auto inserted = mResourceMap.insert(std::make_pair(id, std::move(resource))); 
-			assert(inserted.second); 
-   		}
-		
-		
-		Resource& get(Identifier id)
-		{
-    		auto found = mResourceMap.find(id);
-    		assert(found != mResourceMap.end());
-
-    		return *found->second; 
-		}
-		
-		
-		const Resource& get(Identifier id) const
-		{
-    		auto found = mResourceMap.find(id);
-    		assert(found != mResourceMap.end());
-
-    		return *found->second; 
-		}
-		
+        void load(Identifier id, const std::string& filename);
+        template <typename Parameter>
+    
+        void load(Identifier id, const std::string& filename, const Parameter& secondParam);
+        
+        Resource& get(Identifier id);
+        const Resource& get(Identifier id) const;
 
 	private:
 		std::map<Identifier, std::unique_ptr<Resource> > mResourceMap;
